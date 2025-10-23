68fe96d06c3b34a6048bf4c74e78877b7024fc21
Allan Ratemo <allanisratemo@gmail.com>
2025-10-24 00:37:54 +0300

    Remove unnecessary file

--- src/components/Home.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <h1 className="text-4xl font-bold text-white">Welcome to AllAnime 🎬</h1>
-    </div>
-  );
-}
