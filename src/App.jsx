8d0c707feb7fc4596aea6a5e56e3e3cadb641392
Allan Ratemo <allanisratemo@gmail.com>
2025-10-24 00:37:00 +0300

    Add components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Header from './components/Home';
+import Footer from './components/Footer';
+import AnimeCard from './components/AnimeCard';
+import Navbar from './components/NavBar';
+import SearchBar from './components/SearchBar';
+
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <h1 className="text-4xl font-bold text-white">AllAnime is Live 🎬</h1>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <Header />
+      <SearchBar onSearch={handleSearch} />
+      <main className="flex-grow container mx-auto p-4">
+        <AnimeCard searchQuery={searchQuery} />
+      </main>
+      <Footer />
     </div>
   );
 }
