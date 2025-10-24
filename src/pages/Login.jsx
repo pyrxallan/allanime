472167bc3a29c1b2fd9bdda17e47eacec7e87c13
Allan Ratemo <allanisratemo@gmail.com>
2025-10-24 12:49:15 +0300

    Initial commit Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+import { Mail, Lock, Chrome, Github as GithubIcon } from 'lucide-react';
+
+export default function Login() {
+    
+}
