3cd3608e275f03d524066094a16a7e9c6fb684e6
Allan Ratemo <allanisratemo@gmail.com>
2025-10-24 14:57:24 +0300

    Add navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Search from './pages/Search';
